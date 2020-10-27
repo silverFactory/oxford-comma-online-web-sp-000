0b56004d59ed0f68391d356d68711c136f3c2e11
Will Studabaker <will.studabaker@gmail.com>
2020-10-27 16:38:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ if array.length == 1
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+ else
+   ox_arr = array
+   ox_arr[ox_arr.length - 1] = "and #{ox_arr[ox_arr.length - 1]}"
+   ox_arr = array.join(", ")
+ end
 end
