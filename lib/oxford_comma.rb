55775704f9f28787952275a003d951829b5eb7ca
Will Studabaker <will.studabaker@gmail.com>
2020-10-27 16:32:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
-if array.length == 2
-  array.join(" and ")
-else
- array.join(" ")
+ if array.length == 1
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+ else
+   
+  #do something different with the last and second to last elements in the array
  end
 end
